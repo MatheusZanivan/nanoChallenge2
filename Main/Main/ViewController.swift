ad210edb7438a7c446d7fee0ea9a65eeb4f59d76
vinicius-g-oliv <vinicius.go.oliv@gmail.com>
2022-03-14 15:02:10 -0300

    Adicionado estrutura main

---
+++ Main/Main/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Main
+//
+//  Created by Vinicius Gomes on 14/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
