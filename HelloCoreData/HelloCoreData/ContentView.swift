450946ee3165742354e88a8ed8d38059d1467aa2
rebeca rodrigues <rebecarodrigues@MacBook-Pro-de-rebeca.local>
2022-03-14 14:26:12 -0300

    teste

---
+++ HelloCoreData/HelloCoreData/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloCoreData
+//
+//  Created by rebeca rodrigues on 3/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HelloCoreData/HelloCoreData/HelloCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloCoreDataApp.swift
+//  HelloCoreData
+//
+//  Created by rebeca rodrigues on 3/11/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
