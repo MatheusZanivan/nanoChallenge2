561fe6d2f72a130c125f6914bd772e8d7356625c
cecilia <clopesfornazieri22@snsp.idserve.net>
2022-03-15 15:43:23 -0300

    Tela inicial

---
+++ Main/Main/Botoes.swift
@@ -0,0 +1,8 @@
+//
+//  Botoes.swift
+//  Main
+//
+//  Created by Ceci Fornazieri on 14/03/22.
+//
+
+import Foundation
--- Main/Main/ViewController.swift
+++ Main/Main/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func Todos(_ sender: Any) {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
